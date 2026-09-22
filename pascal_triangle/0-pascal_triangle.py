5622682522c895a21612b8653376621101135692
Nico <nico.ls.asdf@gmail.com>
2026-09-21 23:14:16 -0600

    Add pascals triangle

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -1,1 +1,26 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""Module that defines a function to compute Pascal's triangle."""
+
+
+def pascal_triangle(n):
+    """Return a list of lists of integers representing Pascal's triangle.
+
+    Args:
+        n: number of rows of the triangle.
+
+    Returns:
+        A list of n lists (rows), or an empty list if n <= 0.
+    """
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+    for i in range(1, n):
+        prev_row = triangle[i - 1]
+        new_row = [1]
+        for j in range(1, i):
+            new_row.append(prev_row[j - 1] + prev_row[j])
+        new_row.append(1)
+        triangle.append(new_row)
+
+    return triangle
